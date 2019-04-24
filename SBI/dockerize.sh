3890d1f98315407d125c4739a27065425e39e5b1
Subham Mandal <subhammandal01@gmail.com>
2019-04-24 14:14:52 +0530

    Update dockerize.sh

--- SBI/dockerize.sh
+++ SBI/dockerize.sh
@@ -3,18 +3,18 @@ export PATH="$PATH:/usr/local/bin"
 #docker-compose build maven-app-image-docker
 case $BRANCH_NAME in
   qa)
-    tag=$(git describe | sed 's/-g[0-9a-f]\{7,8\}$//')
-    tag="$BRANCH_NAME:$tag"
+    tags=$(git describe | sed 's/-g[0-9a-f]\{7,8\}$//')
+    tag="$BRANCH_NAME:$tags"
     dockerfile=Dockerfile.develop
     ;;
   develop)
-    tag=$(git describe | sed 's/-g[0-9a-f]\{7,8\}$//')
-    tag="$BRANCH_NAME:$tag"
+    tags=$(git describe | sed 's/-g[0-9a-f]\{7,8\}$//')
+    tag="$BRANCH_NAME:$tags"
     dockerfile=Dockerfile.develop
     ;;
   master)
-    tag=$(git describe | sed 's/-g[0-9a-f]\{7,8\}$//')
-    tag="$BRANCH_NAME:$tag"
+    tags=$(git describe | sed 's/-g[0-9a-f]\{7,8\}$//')
+    tag="$BRANCH_NAME:$tags"
     dockerfile=Dockerfile.develop
     ;;
   *)
