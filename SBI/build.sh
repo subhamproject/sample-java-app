74f017a6809c8097f6f97595f5bf20a177bdca72
Subham Mandal <subhammandal01@gmail.com>
2019-04-30 11:21:56 +0530

    Update build.sh

--- SBI/build.sh
+++ SBI/build.sh
@@ -6,4 +6,4 @@ export GROUPID=$(id -g)
 echo "Running as UID=$USERID, GID=$GROUPID"
 cd $(dirname $0)
 
-docker-compose -f test-bed.yml run --name maven-${BUILD_NUMBER} --rm -w "$WORKSPACE" --entrypoint "mvn deploy -DskipTests" maven-app-build
+docker-compose -f test-bed.yml run --name maven-${BUILD_NUMBER} --rm -w "$WORKSPACE" --entrypoint "mvn -s settings.xml -f POM.xml deploy -DskipTests" maven-app-build
