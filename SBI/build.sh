9307691784c47ce11d35a08a3c96c587f6fb69b3
subhamproject <subhammandal01@gmail.com>
2026-06-03 16:38:59 +0000

    Added new config for testing

--- SBI/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PATH="$PATH:/usr/local/bin"
-export USERID=$(id -u)
-export GROUPID=$(id -g)
-echo "Running as UID=$USERID, GID=$GROUPID"
-cd $(dirname $0)
-
-docker-compose -f test-bed.yml run --name maven-${BUILD_NUMBER} --rm -w "$WORKSPACE" --entrypoint "mvn -s settings.xml -f POM.xml deploy -DskipTests" maven-app-build
