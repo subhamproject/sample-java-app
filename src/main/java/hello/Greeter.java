23f569e7b9a0e425f256ba700df43da30348e504
Subham Mandal <subhammandal01@gmail.com>
2026-06-13 22:00:11 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project Subham PR Demos For Preview Env";
+        return "DevOps Project Subham PR Demos For Preview Envs";
     }
 }
